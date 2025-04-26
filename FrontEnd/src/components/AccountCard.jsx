1d4f0613ae48c1750381b67243165ddccf5497d7
souad <147756485+Souad30@users.noreply.github.com>
2025-04-26 18:42:48 +0200

    errorconnection

---
+++ FrontEnd/src/components/AccountCard.jsx
@@ -0,0 +1,24 @@
+import Button from "../components/Button";
+
+
+/**
+ * Component displaying Account card
+
+ */
+const AccountCard = ({ title, amount, desc }) => {
+    return (
+        <div className="account-item">
+            <div className="account-item__content">
+                <h3 className="account-item__title">{title}</h3>
+                <p className="account-item__amount">{amount}</p>
+                <p className="account-item__desc">{desc}</p>
+            </div>
+            <div className="account-item__cta">
+                <Button btnStyle="btn" label="View transactions" />
+            </div>
+        </div>
+    );
+};
+
+
+export default AccountCard;
---
+++ FrontEnd/src/components/ErrorConnection.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+/**
+* Component for Error Connection
+*/
+const ErrorConnection = () => {
+    return (
+        <div className="error-connection">
+            <h2 className="error-connection__title">Please sign-in to view your profile</h2>
+            <Link to="/login" className="error-connection__link">Go to the Sign-In page</Link>
+        </div>
+    );
+};
+
+export default ErrorConnection;
