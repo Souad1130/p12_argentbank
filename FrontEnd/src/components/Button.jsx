a8612db2bc6660af1b653609786a4e0a460f6150
souad <147756485+Souad30@users.noreply.github.com>
2025-04-26 18:37:05 +0200

    dashboardpage

---
+++ FrontEnd/src/components/Button.jsx
@@ -0,0 +1,14 @@
+
+
+/**
+ * Component displaying button.
+ 
+ */
+const Button = ({ btnStyle, label, onClick }) => {
+    return (
+        <button className={btnStyle} onClick={onClick}>{label}</button>
+    );
+};
+
+
+export default Button;
--- FrontEnd/src/pages/Dashboard.jsx
+++ FrontEnd/src/pages/Dashboard.jsx
@@ -1,10 +1,107 @@
 
+import { accounts } from "../data/accounts";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { editProfil } from "../services/fetchData";
+import { editUser } from "../redux/userSlice";
+import Button from "../components/Button";
+import AccountCard from "../components/AccountCard";
+import ErrorConnection from "../components/ErrorConnection";
+
+/**
+* Dashboard page
+
+*/
 const Dashboard = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [editName, setEditName] = useState(false);
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.user);
+    const token = user.accessToken;
+    const transactions = accounts.find(account => account.userId === user.id)?.transactions || [];
+
+    /**
+    * Handles editing user's first and last name.
     
+    */
+    const editUserName = (e) => {
+        e.preventDefault();
+        setEditName(!editName);
+    };
+
+    /**
+    * Handles saving changes user's first and last name.
+
+    */
+    const saveEditName = async (e) => {
+        e.preventDefault();
+        if (firstName !== "" && lastName !== "" && token) {
+            await editProfil(token, firstName, lastName);
+            dispatch(editUser({
+                firstName: firstName,
+                lastName: lastName,
+            }));
+            setEditName(false);
+        }
+    };
+
+    /**
+    * Handles canceling changes user's first and last name.
+    
+    */
+    const cancelEditName = (e) => {
+        e.preventDefault();
+        if (editName) {
+            setEditName(false);
+        }
+    };
 
     return (
         <>
-           <div>Dashboard</div>
+            {user.signin ? (
+                <div className="dashboard">
+                    <div className="dashboard__head">
+                        {editName ? (
+                            <div className="dashboard__edit">
+                                <h2 className="dashboard__title">Welcome back</h2>
+                                <form className="dashboard__form">
+                                    <div className="dashboard__form-first">
+                                        <input
+                                            type="text"
+                                            id="firstName"
+                                            className="dashboard__form-input"
+                                            onChange={(e) => setFirstName(e.currentTarget.value)}
+                                            defaultValue={user.firstName}
+                                        />
+                                        <Button btnStyle="btn btn--edit-form" label="Save" onClick={saveEditName} />
+                                    </div>
+                                    <div className="dashboard__form-last">
+                                        <input
+                                            type="text"
+                                            id="lastName"
+                                            className="dashboard__form-input"
+                                            onChange={(e) => setLastName(e.currentTarget.value)}
+                                            defaultValue={user.lastName}
+                                        />
+                                        <Button btnStyle="btn btn--edit-form" label="Cancel" onClick={cancelEditName} />
+                                    </div>
+                                </form>
+                            </div>
+                        ) : (
+                            <>
+                                <h2 className="dashboard__title">Welcome back<br />{user.firstName} {user.lastName}</h2>
+                                <Button btnStyle="btn" label="Edit Name" onClick={editUserName}/>
+                            </>
+                        )}
+                    </div>
+                    { transactions.map((transaction) =>
+                        <AccountCard key={transaction.id} title={transaction.title} amount={transaction.amount} desc={transaction.desc} />
+                    )}
+                </div>
+            ) : (
+                <ErrorConnection />
+            )}
         </>
     );
 };
